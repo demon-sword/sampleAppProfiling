f92a267fd1d9c3d86f7171b76984109fb5d01dee
demon-sword <akkisfavourite@gmail.com>
2024-05-28 23:27:55 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+} from 'react-native';
+import { startProfiling, stopProfiling } from 'react-native-release-profiler'
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+import { Component1 } from './src/Component1';
+
+startProfiling()
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Component1 />
+      <Button title={'StopProfiling'} onPress={() => {
+        stopProfiling(true)
+      }}/>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ src/Component1.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { Text, View } from "react-native"
+
+
+export const Component1 = () => {
+    return <View style={{width:200, height: 400}}>
+        <Text>{'Component1'}</Text>
+    </View>
+}
